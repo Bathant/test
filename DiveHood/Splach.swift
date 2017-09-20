c76f18b41ca6a8104168546ebd853733e4e4e8b6
starwallet <nouran@as-MacBook-Pro.local>
2017-09-20 14:48:49 +0200

    Splach

--- DiveHood/Splach.swift
+++ DiveHood/Splach.swift
@@ -15,43 +15,68 @@ class Splach: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-         SetupUi()
         
-        
-          Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.splashTimeOut(sender:)), userInfo: nil, repeats: false)
+        setUpAll()
+    
+        //  Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(self.splashTimeOut(sender:)), userInfo: nil, repeats: false)
         // Do any additional setup after loading the view.
     }
-    func SetupUi(){
-    let image = UIImage(named: "radarlogo")
+  
+    func setUpAll(){
     
-    let LogoImage = UIImageView(frame: CGRect(x: (view.frame.width/2 - ((view.frame.width*0.50)/2)) , y: (view.frame.height/2-((view.frame.width*0.50)/2)), width: (view.frame.width*0.50), height: (view.frame.width*0.50)))
-    //LogoImage.backgroundColor = .gray
-    let label = UILabel(frame: CGRect(x: view.frame.width*0.05, y: view.bounds.midY+view.frame.height*2/25, width: view.frame.width - view.frame.width*0.05*2, height: view.frame.height*1/30))
-    label.text = "The Best diving Booking Services"
-    label.textColor = .white
-    label.textAlignment = .center
-    label.font = UIFont(name: "OpenSans-Bold", size: view.frame.height*1/40)
-    label.baselineAdjustment = .alignCenters
-    view.addSubview(label)
-    //label.backgroundColor = .black
-    let waveimg = #imageLiteral(resourceName: "waves")
+        let Container = UIView(frame: CGRect(x: view.frame.width/2 - (view.frame.width*0.5/2), y: view.frame.midY-(view.frame.width*0.5/2), width: view.frame.width*0.5, height: view.frame.width*0.5))
+        //Container.backgroundColor = .red
+        view.addSubview(Container)
+        SetupContainer(Container)
+        let FooterLabel = UILabel(frame: CGRect(x: view.frame.width*0.10, y: view.bounds.height-view.frame.height*1/40-20, width: view.frame.width - view.frame.width*0.1*2, height: view.frame.height*1/30))
+        FooterLabel.text = "Copyright © 2017 Divey Inc. All rights reserved."
+        FooterLabel.textColor = .white
+        FooterLabel.textAlignment = .center
+        
+        FooterLabel.font = UIFont(name: "OpenSans", size: 12)
+        FooterLabel.adjustsFontSizeToFitWidth = true
+        view.addSubview(FooterLabel)
+        
     
-    let WaveImage = UIImageView(frame: CGRect(x: (view.frame.width/2 - ((LogoImage.frame.width*0.50)/2)) , y: label.frame.origin.y+label.frame.height + view.frame.height*1/40 , width: (LogoImage.frame.width*0.50), height:waveimg.size.height))
-    WaveImage.image = #imageLiteral(resourceName: "waves")
-    view.addSubview(WaveImage)
+    }
+    func SetupContainer(_ contanier:UIView){
+        //backImage
+        let image = UIImage(named: "radarlogo")
+        let LogoImage = UIImageView(frame: CGRect(x: 0  , y: 0, width: contanier.frame.width, height: contanier.frame.height*0.9))
+        LogoImage.image = image
+        LogoImage.contentMode = .scaleAspectFit
+        contanier.addSubview(LogoImage)
+        //WaveImage
+        let waveimg = #imageLiteral(resourceName: "waves")
+        let WaveImage = UIImageView()
+        
+        
+        
+        let label = UILabel(frame: CGRect(x: view.frame.width*0.05, y: contanier.frame.origin.y+contanier.frame.height*0.7, width: view.frame.width - view.frame.width*0.05*2, height: 32))
+        label.text = "The Best diving Booking Services"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont(name: "OpenSans-Bold", size: 16)
+        label.adjustsFontSizeToFitWidth = true
+        
+        
+        
+        print(view.frame.height)
+        
+        label.baselineAdjustment = .alignCenters
+        view.addSubview(label)
+
     
-    LogoImage.image = image
-    LogoImage.contentMode = .scaleAspectFit
-    view.addSubview(LogoImage)
-    let FooterLabel = UILabel(frame: CGRect(x: view.frame.width*0.10, y: view.bounds.height-view.frame.height*1/40-20, width: view.frame.width - view.frame.width*0.1*2, height: view.frame.height*1/30))
-    FooterLabel.text = "Copyright © 2017 Divey Inc. All rights reserved."
-    FooterLabel.textColor = .white
-    FooterLabel.textAlignment = .center
-    
-    FooterLabel.font = UIFont(name: "OpenSans", size: view.frame.height*1/50)
-    FooterLabel.baselineAdjustment = .alignCenters
-    view.addSubview(FooterLabel)
-    
+        
+        
+        //WaveImage.backgroundColor = .black
+        WaveImage.frame.origin.y = contanier.frame.height*0.92
+        WaveImage.frame.origin.x = contanier.frame.width/2 - waveimg.size.width/2
+        WaveImage.frame.size = waveimg.size
+        WaveImage.image = #imageLiteral(resourceName: "waves")
+        contanier.addSubview(WaveImage)
+        
+        
     
     }
     override func didReceiveMemoryWarning() {
