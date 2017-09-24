a8a96093b1610d34b5af9103ffab442d966aeb59
starwallet <nouran@as-MacBook-Pro.local>
2017-09-24 15:37:19 +0200

    Finished Tab Bar Controller

--- DiveHood/CustomTabBarController.swift
+++ DiveHood/CustomTabBarController.swift
@@ -17,14 +17,15 @@ extension UITabBar{
                 
                 item.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.white], for: .normal)
                 item.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.white], for: .selected)
+               
                 
             }
         }
     }
 }
 extension UIImage {
-    func tintedWithLinearGradientColors(colorsArr: [CGColor?]) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale);
+    func tintedWithLinearGradientColors(colorsArr: [CGColor?], size : CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, self.scale);
         let context = UIGraphicsGetCurrentContext()
         context!.translateBy(x: 0, y: self.size.height)
         context!.scaleBy(x: 1.0, y: -1.0)
@@ -36,7 +37,7 @@ extension UIImage {
         
         let colors = colorsArr as CFArray
         let space = CGColorSpaceCreateDeviceRGB()
-        let gradient = CGGradient(colorsSpace: space, colors: colors, locations: [0.0 , 1.0])
+        let gradient = CGGradient(colorsSpace: space, colors: colors, locations: nil)
         
         // Apply gradient
         
@@ -59,29 +60,46 @@ extension UIImage
         color.setFill()
         UIRectFill(rect)
         let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-      
-        let img = image.tintedWithLinearGradientColors(colorsArr: [UIColor(red:0.05, green:0.15, blue:0.24, alpha:1.0).cgColor , Colors().gradientColor2.cgColor])
+        let img = image.tintedWithLinearGradientColors(colorsArr: [UIColor(red:0.05, green:0.15, blue:0.24, alpha:1.0).cgColor , Colors().gradientColor2.cgColor], size : size )
         UIGraphicsEndImageContext()
         return img
     }
 }
-class CustomTabBarController: UITabBarController {
+class CustomTabBarController: UITabBarController  {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.navigationItem.title = "LiveaboardsList"
-        self.tabBar.barTintColor = UIColor(red:0.05, green:0.15, blue:0.24, alpha:0.9)
-       
-        setupUi()
-         self.tabBar.inActiveTintColor()
-        let numberOfItems = CGFloat(self.tabBar.items!.count)
-        let tabBarItemSize = CGSize(width: self.tabBar.frame.width / numberOfItems, height: tabBar.frame.height + 30)
-        tabBar.selectionIndicatorImage = UIImage.imageWithColor(color: UIColor.brown, size: tabBarItemSize).resizableImage(withCapInsets: .zero)
+         setupUi()
+        SetTabBar()
+        
+        
+        
+        
         
     }
     
     
+    func SetTabBar(){
     
+        self.tabBar.barTintColor = UIColor(red:0.05, green:0.15, blue:0.24, alpha:0.9)
+        UITabBarItem.appearance().titlePositionAdjustment = UIOffsetMake(0.0, -2.0)
+        self.tabBar.inActiveTintColor()
+        self.tabBar.tintColor = .white
+        
+        
+        let numberOfItems = CGFloat(self.tabBar.items!.count)
+        let tabBarItemSize = CGSize(width: self.tabBar.frame.width/numberOfItems, height: tabBar.frame.height+40 )
+        tabBar.selectionIndicatorImage = UIImage.imageWithColor(color: UIColor.brown, size: tabBarItemSize).resizableImage(withCapInsets: .init())
+      
+         var items = self.tabBar.items
+        items?[0].titlePositionAdjustment = UIOffsetMake(0.0, -10.0)
+        items?[0].setTitleTextAttributes([NSFontAttributeName : UIFont(name : "OpenSans",size: 12)], for: .normal)
+        items?[0].imageInsets = UIEdgeInsetsMake(-10, 0, 10, 0)
+        
+
+        
+    
+    }
     func setupUi(){
         setbackground()
         let LiveBoards = UIViewController()
@@ -120,7 +138,23 @@ class CustomTabBarController: UITabBarController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        var items = self.tabBar.items
+        
+        items?[self.selectedIndex].titlePositionAdjustment = UIOffsetMake(0.0, -2.0)
+        items?[self.selectedIndex].setTitleTextAttributes([NSFontAttributeName : UIFont(name : "OpenSans",size: 10), NSForegroundColorAttributeName: UIColor.white], for: .normal)
+        items?[self.selectedIndex].imageInsets = UIEdgeInsetsMake(0, 0, 0, 0)
+        
+        var index = items?.index(of: item)
+     
+        items?[index!].titlePositionAdjustment = UIOffsetMake(0.0, -10.0)
+        items?[index!].setTitleTextAttributes([NSFontAttributeName : UIFont(name : "OpenSans",size: 12)], for: .normal)
+        items?[index!].imageInsets = UIEdgeInsetsMake(-10, 0, 10, 0)
+       
+      
+        
+
+    }
     /*
      // MARK: - Navigation
      
